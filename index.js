4f7350fb717355e2f38ad3f44faa34840e6fcb92
Benjamin Bowes <bbowes999@gmail.com>
2018-01-24 20:53:39 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,27 @@
+function theBeatlesPlay(musicians, instruments){
+  var array = [];
+
+for (var i = 0; i < musicians.length; i++){
+
+array.push (`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return array
+
+}
+function johnLennonFacts(facts){
+   var array = [];
+var i = 0;
+while (i < facts.length){
+  array[i] = facts[i] + "!!!"
+i++;
+}
+return array
+}
+function iLoveTheBeatles(n){
+  var array = [];
+  do {array[n] + "I love the Beatles!"}
+  while (array.length<15);{
+  }
+  
+  return array;
+}
